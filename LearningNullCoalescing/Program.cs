0482e8261079201f095f7d06ede05dcd1f8a1cb5
puglisac <alan.c.puglisi@gmail.com>
2020-11-18 14:54:55 -0600

    first practice with null coalescing

---
+++ LearningNullCoalescing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LearningNullCoalescing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "hello";
+            var s2 = s ?? "default";
+
+            Console.WriteLine("s equals: "+s);
+            s = null;
+            Console.WriteLine("now s equals: "+s);
+            Console.WriteLine("s2 equals: "+s2);
+        }
+    }
+}
