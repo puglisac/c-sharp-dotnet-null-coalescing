706cab9e4647c7558d06f9517998189e5eb68abe
puglisac <alan.c.puglisi@gmail.com>
2020-11-18 15:04:32 -0600

    ready to push

--- LearningNullCoalescing/Program.cs
+++ LearningNullCoalescing/Program.cs
@@ -7,10 +7,9 @@ namespace LearningNullCoalescing
         static void Main(string[] args)
         {
             var s = "hello";
-            var s2 = s ?? "default";
-
             Console.WriteLine("s equals: "+s);
             s = null;
+            var s2 = s ?? "default";
             Console.WriteLine("now s equals: "+s);
             Console.WriteLine("s2 equals: "+s2);
         }
